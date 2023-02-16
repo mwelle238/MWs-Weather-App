356865066fa9ea29a46d6d9ade8fc61cb603ddb9
Michael Welle <michaelpwelle@gmail.com>
2023-02-15 21:08:51 -0800

    Finished JS, Tweaked reactive layout, ready to deploy

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -8,6 +8,7 @@ var state;
 var country;
 const iconURLprefix = 'http://openweathermap.org/img/wn/';
 var resultsEl = $('#results');
+var instructionEl = $('#instruction');
 var placeEL = $('#place');
 var weatherCards = $('.weather-card');
 var historyButtons = $('.search-history');
@@ -17,22 +18,24 @@ console.log(placeEL);
 
 function getForecast(){
     if(city){
-    fetch(geoURL + city + "&limit=5" + APIkey)  // Search by city to get coordinates.
+    fetch(geoURL + city + "&limit=1" + APIkey)  // Search by city to get coordinates.
      .then(function (response) {
        return response.json();
      })
         .then(function (data) {
-            if(data.length == 0){
+            if(data.length == 0){  // if API doesn't return anything, do nothing
                 return;
             }
-            saveSearch(city);
+            saveSearch(city);  //add to search history
             lon = data[0].lon;
             lat = data[0].lat;
             city = data[0].name;
             state = data[0].state;
             country = data[0].country;
             placeEL[0].textContent = city + ", " + state + " " + country;
-            resultsEl.first().removeClass('d-none'); 
+            instructionEl.first().addClass('d-none'); // hide instructions
+            resultsEl.first().removeClass('d-none'); // display weather results
+            
             
             fetch(weatherURL + 'lat=' + lat + '&lon=' + lon + "&units=imperial&" + APIkey)  // takes longitude and latitude data from the city search to call the weather API
             .then(function (response) {
@@ -41,37 +44,57 @@ function getForecast(){
             .then(function (data) {
                 //console.log(data);
 
+              
+
               for(let i=0; i<weatherCards.length; i++){
-                weatherCards[i].children[1].children[0].attributes[1].nodeValue = iconURLprefix + data.daily[i].weather[0].icon + (i===0 ? '@2x.png' : '.png');
+                // get wind speed and direction
+                let windInfo = data.daily[i].wind_speed;
+                switch (Math.floor((data.daily[i].wind_deg+23)/45)) {
+                  case 0: case 8: windInfo += ' N'; break;
+                  case 1: windInfo += ' NE'; break;
+                  case 2: windInfo += ' E'; break;
+                  case 3: windInfo += ' SE'; break;
+                  case 4: windInfo += ' S'; break;
+                  case 5: windInfo += ' SW'; break;
+                  case 6: windInfo += ' W'; break;
+                  case 7: windInfo += ' NW'; break;
+                  default:
+                }
+                // display weather info on the cards
+                weatherCards[i].children[1].children[0].attributes[1].nodeValue = iconURLprefix + data.daily[i].weather[0].icon + '.png';
                 weatherCards[i].children[1].children[1].children[0].textContent = data.daily[i].temp.max;
                 weatherCards[i].children[1].children[1].children[2].textContent = data.daily[i].temp.min;
                 weatherCards[i].children[1].children[1].children[4].textContent = data.daily[i].humidity;
-                weatherCards[i].children[1].children[1].children[6].textContent = data.daily[i].weather[0].description;
+                weatherCards[i].children[1].children[1].children[6].textContent = windInfo;
+                weatherCards[i].children[1].children[1].children[8].textContent = data.daily[i].weather[0].description;
                 weatherCards[i].children[0].textContent = dayjs.unix(data.daily[i].sunrise).format('MMM D, YYYY');
               }    
             });
      })
     }
 }
+
+// update the search history with the new search and save it to localStorage
 function saveSearch(search){
     search = search.charAt(0).toUpperCase() + search.slice(1).toLowerCase();
-    if(!searchHistory) searchHistory = [];
+    if(!searchHistory) searchHistory = [];  //make sure searchHistory is defined
     searchHistory.splice(0,0,search) //add new search to beginning
     for (let i=1; i<searchHistory.length; i++){
         if (search === searchHistory[i]){
-            searchHistory.splice(i,1);
+            searchHistory.splice(i,1); // if a search button was pressed, would show up twice in history, so remove old one
             break;
         }
     }
-    while (searchHistory.length > 5){
-        searchHistory.pop();
+    while (searchHistory.length > 8){
+        searchHistory.pop();  // only hold 8 most recent searches
     }
-    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory)); 
     showHistory();
 }
 
+// search history buttons only show if they exist
 function showHistory(){
-    if(!searchHistory) searchHistory = [];
+    if(!searchHistory) searchHistory = [];  
     console.log(searchHistory);
     for (let i=0; i<searchHistory.length; i++ ){
       historyButtons[i].style.visibility = "visible";
@@ -82,6 +105,7 @@ function showHistory(){
 var cityFormEl = document.querySelector('#city-search');
 var cityInputEl = document.querySelector('#city');
 
+// weather button clicked
 function formSubmitHandler(event) {
   event.preventDefault();
 
@@ -92,6 +116,7 @@ function formSubmitHandler(event) {
   }
 };
 
+// search history button pressed 
 function reloadHistory(event){
     event.preventDefault();
     city = this.textContent;
